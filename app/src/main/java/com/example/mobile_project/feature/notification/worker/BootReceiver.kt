43bab0fa6a7c50275de5a86f12198c2da42929f9
ChiTrungVo <vochitrung212@gmail.com>
2026-06-05 00:01:45 +0700

    Update BootReceiver.kt

--- app/src/main/java/com/example/mobile_project/feature/notification/worker/BootReceiver.kt
+++ app/src/main/java/com/example/mobile_project/feature/notification/worker/BootReceiver.kt
@@ -7,8 +7,7 @@ import android.content.Intent
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            // Đọc lại settings đã lưu rồi schedule lại
-            // Tạm thời để trống — khi tích hợp Appwrite thì gọi load settings ở đây
+            
         }
     }
 }
